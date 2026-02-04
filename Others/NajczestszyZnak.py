c178f0632ff5fc1247532b9d42ded4c133b371d8
KrajziXXX <127192893+KrajziXXX@users.noreply.github.com>
2026-02-04 20:00:25 +0100

    zadania

---
+++ Others/NajczestszyZnak.py
@@ -0,0 +1,11 @@
+def najczestszy_znak(tekst):
+    max_znak = ""
+    max_licznik = 0
+
+    for znak in tekst:
+        licznik = tekst.count(znak)
+        if licznik > max_licznik:
+            max_licznik = licznik
+            max_znak = znak
+
+    return max_znak
