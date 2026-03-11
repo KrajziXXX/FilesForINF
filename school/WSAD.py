455cbba430ee9cf80fa1e65182c87c52b0fb3604
KrajziXXX <127192893+KrajziXXX@users.noreply.github.com>
2026-03-11 18:32:34 +0100

    Implement goal position and game end logic
    Added a goal position and end game conditions.

--- school/WSAD.py
+++ school/WSAD.py
@@ -8,8 +8,13 @@ def main():
     pos = [random.randint(0, x-1),random.randint(0, y-1)]
     print(pos)
 
+    goal = [random.randint(0, x-1), random.randint(0, y-1)]
+    while goal == pos:
+        goal = [random.randint(0, x-1), random.randint(0, y-1)]
+
     map = [[None] * y for _ in range(x)]
     map[pos[0]][pos[1]] = "X"
+    map[goal[0]][goal[1]] = "G"
 
     while True: 
 
@@ -19,7 +24,7 @@ def main():
         d = y - pos[1] - 1
 
         for row in map:
-            print(" ".join("[X]" if el == "X" else "[]" for el in row))
+            print(" ".join("[X]" if el == "X" else "[G]" if el == "G" else "[]" for el in row))
 
         ruch = input("W/S/A/D")
         if(ruch.upper() == "W" and w != 0):
@@ -30,9 +35,21 @@ def main():
             pos [1]-= 1
         if(ruch.upper() == "D" and d != 0):
             pos [1]+= 1
-        
+        if (ruch.upper() not in ["W", "S", "A", "D"]):
+            print("Niepoprawny ruch")
+            continue
+        if(w == 0 and s == 0 and a == 0 and d == 0):
+            print("Nie można się ruszyć")
+            break
+        if(ruch.upper() == "E"):
+            print("Koniec gry")
+            break
+        if pos == goal:
+            print("Gratulacje, wygrałeś!")
+            break
         map = [[None] * y for _ in range(x)]
         map[pos[0]][pos[1]] = "X"
+        map[goal[0]][goal[1]] = "G"
 
 
         
